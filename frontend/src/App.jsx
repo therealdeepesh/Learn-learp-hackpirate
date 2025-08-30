7555885329ec30c9cd81cb8de09d82fc8a51ef74
Bhushan <pardeepdpolice6@gmail.com>
2025-08-30 13:40:11 +0530

    initial routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Home from './pages/Home'
+import About from './pages/About'
+import Courses from './pages/Courses'
+import Profile from './pages/Profile'
+import Feed from './pages/Feed'
+import Quiz from './pages/Quiz'
+import NotFound from './pages/NotFound'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import { ToastContainer } from 'react-toastify'
+import Layout from './layouts/Layout'
+import { auth } from "../config/firebase";
+import { onAuthStateChanged} from "firebase/auth";
+import CourseDetail from "./components/CourseDetail";
+import Chat from "./pages/Chat";
+import TopicDetail from "./components/TopicDetails";
+import PdfQuizGenerator from "./components/PdfQuizGenerator";
+import DetailedAnalytics from "./components/DetailedAnalytics";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navigate = useNavigate();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen bg-[#f6fbf6] ">
+      {/* <Sidebar /> */}
+      <Routes path="/" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/pdf-to-quiz" element={<PdfQuizGenerator />} />
+          <Route path="/courses/introduction-to-react" element={<CourseDetail />} />
+          <Route path="/topics/:topicId" element={<TopicDetail />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/detailed-analytics" element={<DetailedAnalytics />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/chat" element={<Chat />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer />
+    </div>
   )
 }
 
